922ec9d03fd4a5c53724aaa42769a3d6549cec68
Nick Ruest <ruestn@gmail.com>
2011-01-04 14:40:36 -0500

    John & Nick initial discussion

--- cloud computing and libraries.mm
+++ cloud computing and libraries.mm
@@ -1,4 +1,42 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1294168120223" ID="Freemind_Link_1635245266" MODIFIED="1294168144168" TEXT="cloud computing and libraries"/>
+<node CREATED="1294168120223" ID="Freemind_Link_1635245266" MODIFIED="1294169903730" TEXT="Local clouds in the sky with diamonds?">
+<node CREATED="1294168539522" ID="Freemind_Link_374954039" MODIFIED="1294168543726" POSITION="left" TEXT="John">
+<node CREATED="1294168483257" ID="_" MODIFIED="1294169055237" TEXT="Define: cloud"/>
+<node CREATED="1294168608480" ID="Freemind_Link_234733625" MODIFIED="1294168621859" TEXT="Clouds - Local">
+<node CREATED="1294168669318" ID="Freemind_Link_152674649" MODIFIED="1294168680315" TEXT="Why local?"/>
+<node CREATED="1294168688865" ID="Freemind_Link_116056757" MODIFIED="1294168693337" TEXT="Products">
+<node CREATED="1294168583657" ID="Freemind_Link_184824817" MODIFIED="1294168590412" TEXT="glusterfs"/>
+<node CREATED="1294168591288" ID="Freemind_Link_117345317" MODIFIED="1294168594724" TEXT="hadoop"/>
+<node CREATED="1294168784770" ID="Freemind_Link_1982021355" MODIFIED="1294168800678" TEXT="Sun grid engine"/>
+</node>
+<node CREATED="1294169027435" ID="Freemind_Link_681945786" MODIFIED="1294169033952" TEXT="Why not remote?"/>
+</node>
+</node>
+<node CREATED="1294168554210" ID="Freemind_Link_477102169" MODIFIED="1294168556254" POSITION="right" TEXT="Nick">
+<node CREATED="1294168559657" ID="Freemind_Link_1094956907" MODIFIED="1294169042391" TEXT="Products">
+<node CREATED="1294168595329" ID="Freemind_Link_951692985" MODIFIED="1294168598100" TEXT="cassandra"/>
+<node CREATED="1294168598976" ID="Freemind_Link_705687165" MODIFIED="1294168604612" TEXT="eucaluptus"/>
+<node CREATED="1294168740916" ID="Freemind_Link_274157032" MODIFIED="1294168885651" TEXT="cloud crowd">
+<node CREATED="1294169105280" ID="Freemind_Link_703339639" MODIFIED="1294169108108" TEXT="define"/>
+<node CREATED="1294169109872" ID="Freemind_Link_1637245698" MODIFIED="1294169114003" TEXT="how we used it">
+<node CREATED="1294169193270" ID="Freemind_Link_1927271888" MODIFIED="1294169207184" TEXT="why to we thought to cloud it">
+<node CREATED="1294169207421" ID="Freemind_Link_850296060" MODIFIED="1294169222763" TEXT="imagemagick jp2 conversion slowness"/>
+</node>
+</node>
+<node CREATED="1294169114920" ID="Freemind_Link_1712896468" MODIFIED="1294169119707" TEXT="how it failed">
+<node CREATED="1294169140911" ID="Freemind_Link_830679217" MODIFIED="1294169144171" TEXT="seg faulted"/>
+<node CREATED="1294169145455" ID="Freemind_Link_1144184261" MODIFIED="1294169175730" TEXT="ran slow (imagemagick jp2 library)"/>
+<node CREATED="1294169228484" ID="Freemind_Link_736427861" MODIFIED="1294169242431" TEXT="write our own ruby scripts"/>
+<node CREATED="1294169243531" ID="Freemind_Link_1076527619" MODIFIED="1294169262407" TEXT="clouds nodes dropping out">
+<node CREATED="1294169271523" ID="Freemind_Link_317013837" MODIFIED="1294169279558" TEXT="nodes taking too long to answer"/>
+<node CREATED="1294169281746" ID="Freemind_Link_805606381" MODIFIED="1294169298687" TEXT="modify gem *&amp; recompile"/>
+</node>
+<node CREATED="1294169263611" ID="Freemind_Link_1495232787" MODIFIED="1294169332405" TEXT="output was sporatic (nodes dropping)"/>
+<node CREATED="1294169333617" ID="Freemind_Link_281721271" MODIFIED="1294169333617" TEXT=""/>
+</node>
+</node>
+</node>
+</node>
+</node>
 </map>
