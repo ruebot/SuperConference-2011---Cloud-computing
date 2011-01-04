8b777fa6974d3270d5412d56cbf0419a36b8bbd5
Nick Ruest <ruestn@gmail.com>
2011-01-04 14:10:53 -0500

    Initial commit

---
+++ cloud computing and libraries.mm
@@ -0,0 +1,4 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1294168120223" ID="Freemind_Link_1635245266" MODIFIED="1294168144168" TEXT="cloud computing and libraries"/>
+</map>
